5a791b15cbf8d495666573d39f099ee8120f8227
ryanc0714 <52945383+ryanc0714@users.noreply.github.com>
2023-03-06 19:55:44 +0000

    sds

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ print(response)
 '''
 #print 200 meaning request was succsesful
 
-url = "http://api.weatherapi.com/v1/current.json?key=9797749cd6764933844220709230602&q=London&aqi=no"
+url = "http://api.weatherapi.com/v1/current.json?key=9797749cd6764933844220709230602&q=Seattle&aqi=no"
 
 #signed up for weatherapi account and using this url to practice before starting on real project. 
 #created a google doc to learn w me. 
@@ -15,10 +15,18 @@ response = requests.get(url)
 json = response.json()
 ##cant use .location
 print(response)
+
 print(json["location"])
 #output is the location of this weather information is the city of london specifically lat:51.52 lon:-0.11 at the current time
 temperature = json["current"]["temp_f"]
-print(temperature)
+print("current temperature in Seattle in Farenheight is : " + str(temperature))
+
+humidity = json['current']["humidity"]
+print("current huidity is" + str(humidity))
+wind = json['current']["wind_mph"]
+print("current wind speed is " + str(wind))
+
+
 #gives me exactly current temperature right now in london pretty cool
 #ryan g's job:create a plot of this using the past weeks temp in london and graph it out
 
