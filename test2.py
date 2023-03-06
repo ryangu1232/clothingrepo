8fe56b94a15bb50b04e08ced566fae9e5da9f3e2
ryangu1232 <113143244+ryangu1232@users.noreply.github.com>
2023-03-06 20:27:13 +0000

    FINAL VERSION

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@
 import requests 
 import json 
 
-responses = requests.get('https://coronavirus-api.net/get_all?min_date=2020-03-22&states=["Washington", "Seattle"]').text
+responses = requests.get('https://api.covid19api.com/summary').text
 
 ##print(responses)
 
@@ -19,7 +19,7 @@ country_list = []
 for country_info in responseinfo['Countries']:
  country_list.append([country_info['Country'], country_info['TotalDeaths']])
 
-#print(country_list)
+print(country_list)
 
 import pandas as pd
 
