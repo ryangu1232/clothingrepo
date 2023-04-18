095bec507db1d21fc112b64c0dac7ede6a98e11d
ryangu1232 <113143244+ryangu1232@users.noreply.github.com>
2023-04-18 03:31:36 +0000

    testing testing

--- pictures.py
+++ pictures.py
@@ -1,11 +1,11 @@
 import requests
 
-image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Main_Building_at_the_University_of_Notre_Dame.jpg/437px-Main_Building_at_the_University_of_Notre_Dame.jpg'
+image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4rynfs1wJGEZ5iM4LYKkMesf8itB4r-5GIdHW9MfFBopmYTMKWIhx8HJwqI7ggGXZml0:https://a.espncdn.com/combiner/i%3Fimg%3D/i/teamlogos/nfl/500/sea.png&usqp=CAU'
     #image  = input('please input your desired image link')
     #still working on how to ask for input of a link
     #we will learn how to pull an image link and display it somewhere next
 
 picture = requests.get(image)
 print(picture.content)
-with open('ryanc:\notredame.png','wb') as f:
+with open('ryang:\abc.png','wb') as f:
     f.write(picture.content)
