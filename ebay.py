fc5570418e459c4381b55d3d182ffcb745299072
ryanc0714 <52945383+ryanc0714@users.noreply.github.com>
2023-04-18 04:46:50 +0000

    bassically added a lot of statistical analysis in ebay.py and took a long time to convert 'cost' values into floats that I can play around with.
    Next doing the same with poshmark should be faster.

--- ebay.py
+++ ebay.py
@@ -1,32 +1,82 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 ebay_url = 'https://www.ebay.com/sch/yeezy-gap-hoodie'
 response = requests.get(ebay_url)
 
-#print(response) working = 200
 soup = BeautifulSoup(response.text, "html.parser")
-##print(soup) succsesful in bringing html code from ebay website
 
-tagscost = soup.findAll(class_ = "s-item__price")
-tagsname = soup.findAll(class_ = "s-item__title")
-secondhand = soup.findAll(class_ = "SECONDARY_INFO")
+tagscost = soup.findAll(class_="s-item__price")
+tagsname = soup.findAll(class_="s-item__title")
+secondhand = soup.findAll(class_="SECONDARY_INFO")
 
 yeezy_gaplisting = []
-for i in range(1,(len(tagsname))):
+for i in range(1, len(tagsname)):
     yeezy_gaplisting.append(tagsname[i].text)
-print(yeezy_gaplisting)
 
 yeezy_gaplistingcost = []
-for i in range(1,(len(tagsname))):
-    yeezy_gaplistingcost.append(tagscost[i].text)
-print(yeezy_gaplistingcost)
+for i in range(1, len(tagsname)):
+    yeezy_gaplistingcost.append(str(tagscost[i]))
+#print(yeezy_gaplistingcost)
+
+import re
+
+prices = []
+
+for price in yeezy_gaplistingcost:
+    price_value = re.findall('\d+\.\d+', price) # find all numbers with decimal points
+    if price_value:
+        prices.append(float(price_value[0])) # convert string to float and append to prices list
+
+print(prices)
+
 
 yeezy_gaplistinguse = []
 for i in range(1,(len(tagsname))):
     yeezy_gaplistinguse.append( secondhand[i].text)
-print(yeezy_gaplistinguse)
+#print(yeezy_gaplistinguse)
 
 
-###import pandas as pd
+
 #df = pd.DataFrame(yeezy_gaplisting, columns=['yeezy_gaplisting'])
 #df.to_csv('yeezy-gap-listing-ebay.csv')
+
+
+df = pd.DataFrame(np.column_stack([yeezy_gaplisting, prices, yeezy_gaplistinguse]), columns=['Item', 'Cost', 'Wear'])
+df["Cost"] = pd.to_numeric(df["Cost"], errors="coerce")
+df.to_csv('yezy-ebay.xls')
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(df.head())
+
+#base analytics
+
+print(df.describe())
+wear_counts = df['Wear'].value_counts()
+print(wear_counts)
+
+
+
+# create a boxplot
+fig, ax = plt.subplots()
+plt.boxplot([df[df['Wear'] == 'Brand New']['Cost'], 
+             df[df['Wear'] == 'New']['Cost'], 
+             df[df['Wear'] == 'Pre-owned']['Cost']],
+            labels=['Brand New', 'New', 'Pre-owned'])
+
+# set the title and axis labels
+plt.title('Cost Distribution by Wear')
+plt.xlabel('Wear')
+plt.ylabel('Cost')
+
+# save the plot to a file
+fig.savefig('wear_cost_boxplot.png')
+
+# calculate the correlation coefficient between 'Cost' and 'Wear'
+corr_coef = df['Cost'].corr(df['Wear'], method='spearman')
+print(f"Correlation coefficient between 'Cost' and 'Wear': {corr_coef:.2f}")
