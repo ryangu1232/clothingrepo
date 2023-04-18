c43ee9b9eeb814429cc037fdc2d08937d4c9bbce
ryangu1232 <113143244+ryangu1232@users.noreply.github.com>
2023-04-18 09:39:50 +0000

    Last Commit

--- pictures/ebayimages.py
+++ pictures/ebayimages.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 #creates api endpoint when calls are fufilled
-api_endpoint = 'https://svcs.ebay.com/services/search/FindingService/v1'
+endpoint = 'https://svcs.ebay.com/services/search/FindingService/v1'
 
-# eBay API credentials
+#Ebay API key
 app_id = 'Ebay API Key (currently pending approval)'
 
 
-# Set up the request headers
+#Headers for the request
 headers = {
      'X-EBAY-SOA-OPERATION-NAME': 'findItemsAdvanced',
      'X-EBAY-SOA-SERVICE-NAME': 'FindingService',
@@ -18,30 +18,30 @@ headers = {
      'X-EBAY-SOA-SECURITY-APPNAME': app_id
 }
 
-# Set up the request body
+#Setse up request
 payload = {
-    'keywords': 'iphone', # replace with your search keywords
+    'keywords': 'iphone', #replace with search terms
     'paginationInput': {
-        'entriesPerPage': 10, # replace with your desired number of results
+        'entriesPerPage': 10, #replace with number of search results
         'pageNumber': 1
      },
     'outputSelector': ['PictureURLLarge'] # retrieve only large pictures
 }
 
 # Make the API request
-response = requests.post(api_endpoint, headers=headers, json=payload)
+response = requests.post(endpoint, headers=headers, json=payload)
 
 # Parse the JSON response
 data = json.loads(response.text)
 
-# Extract the picture URLs from the response
+#extracts picture url's
 picture_urls = []
 for item in data['findItemsAdvancedResponse'][0]['searchResult'][0]['item']:
     if 'galleryURL' in item:
         picture_urls.append(item['galleryURL'][0])
 
 
-# Download the pictures
+#downloads pictures
 for i, picture_url in enumerate(picture_urls):
     response = requests.get(picture_url)
     with open(f'image_{i}.jpg', 'wb') as f:
