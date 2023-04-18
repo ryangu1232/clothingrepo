93b2cffaf438c8d3e75035a9a94eb1c0ac7f727c
ryanc0714 <52945383+ryanc0714@users.noreply.github.com>
2023-04-18 03:04:06 +0000

    ebay turned data output into segregated lists
    merge.py fixed minor errors

--- merge.py
+++ merge.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # reading csv files
-data1 = pd.read_csv('nike-listing-ebay.csv')
+data1 = pd.read_csv('yeezy-listing-ebay.csv')
 data2 = pd.read_csv('yeezy-gap-listing-ebay.csv')
 
 # using merge function by setting how='outer'
@@ -10,4 +10,5 @@ output4 = pd.merge(data1, data2,
 				how='outer')
 
 # displaying result
-print(output4)
+
+output4.to_csv('merge.csv')
