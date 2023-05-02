0941d6cf3ffecb17ea97cdf51cfa46bdbc23e0ca
ryanc0714 <52945383+ryanc0714@users.noreply.github.com>
2023-05-02 16:49:06 +0000

    cleaned up stuff and added stuff

--- merge.py
+++ merge.py
@@ -1,16 +1,40 @@
 import pandas as pd
+import numpy as np
 
-# reading csv files
-data1 = pd.read_csv('yeezy-listing-ebay.csv')
-data2 = pd.read_csv('yeezy-gap-listing-ebay.csv')
+data1 = pd.read_csv('nike-poshmark.xls')
+data2 = pd.read_csv('yezy-ebay.xls')
 
 
-# using merge function by setting how='outer'
-output4 = pd.merge(data1, data2, #data3,
-				on='yeezy_gaplisting',
-				#right_on = 'grailed_listing',
-				how='outer')
+output4 = pd.merge(data1, data2, how='outer')
 
-# displaying result
 
 output4.to_csv('merge.csv')
+
+
+df = pd.read_csv('merge.csv')
+
+df = df.sort_values(by='Cost', ascending=False)
+
+df.to_csv('mergecost.csv')
+
+print(df)
+print(df.describe())
+cf = pd.read_csv('yezy-ebay.xls')
+print(cf.describe())
+
+df_mean_cost = df['Cost'].mean()
+cf_mean_cost = cf['Cost'].mean()
+
+# Print the results
+if df_mean_cost > cf_mean_cost:
+    print("On average, items in df cost more than items in cf")
+else:
+    print("On average, items in cf cost more than items in df")
+
+price_limit = float(input("Enter a price limit: "))
+
+filtered_df = df[df['Cost'] < price_limit]
+
+filtered_df.to_csv('filtered_data.csv', index=False)
+
+print(filtered_df)
