a2df319aa90db8e4b7eba87bb72cac827e985b3c
ryangu1232 <113143244+ryangu1232@users.noreply.github.com>
2023-05-31 07:21:55 +0000

    updated workingpictures.py try/catch exception

--- pictures/WORKINGpictures.py
+++ pictures/WORKINGpictures.py
@@ -1,35 +1,64 @@
 import requests
+
 import pandas as pd
+
+
+
 
 data2 = pd.read_csv('merge.csv')
 
+
+
+
 row_number = int(input('Enter the row number: '))
 
+
+
+
 # Check if there is a row here
+
 if row_number >= 0 and row_number < len(data2):
-    
+
+   
+
     row_data = data2.iloc[row_number]
 
+
+
+
     # Get the image link from the row data
+
     image_link = row_data['ImgLink']
 
+
+
+
     # Output the image link
+
     print(image_link)
+
+   
+
+    try:
+
+        picture = requests.get(image_link)
+
+       
+
+        if picture.status_code == 200:
+
+            with open('imagefordemo/notredame.png', 'wb') as f:
+
+                f.write(picture.content)
+
+        else:
+
+            print('No image available for this value')
+
+    except requests.exceptions.MissingSchema:
+
+        print('Invalid URL or no image available')
+
 else:
+
     print('Invalid row number. Please try again.')
-
-
-
-image = image_link
-
-    #still working on how to ask for input of a link
-
-     #we will learn how to pull an image link and display it somewhere next
-
-
-picture = requests.get(image)
-
-print(picture.content)
-
-with open('imagefordemo:\notredame.png','wb') as f:
-    f.write(picture.content)
